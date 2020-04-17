0e03ec9ea4766ef817794f6d4523bb8c684f4843
潘文伟 <873168555@qq.com>
2020-04-17 14:50:56 +0800

    button

--- packages/Button/index.vue
+++ packages/Button/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <button class="p-button"  :class="`p-button-${type}`" @click="bindClick" type="button">
+  <button
+  class="p-button"
+  :disabled="disabled"
+  :class="[`p-button-${type}`,{
+    'is-disabled':disabled
+  }]"
+  @click="bindClick" type="button"
+  >
      <span>
        <slot></slot>
      </span>
@@ -13,6 +20,10 @@ export default {
     type: {
       type: String,
       default: 'default'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
     }
   },
   methods: {
@@ -23,7 +34,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 .p-button {
     cursor: pointer;
     display: inline-block;
@@ -32,7 +43,6 @@ export default {
     -webkit-user-select: none;
     -webkit-appearance: none;
     outline: none;
-    border: none;
     border: 1px solid #dcdddd;
     background: #fff;
     color: #221e1f;
@@ -42,17 +52,117 @@ export default {
     transition: .1s;
     font-weight: 500;
     margin: 0;
-}
-
-.p-button:focus, .p-button:hover {
-    color: #409eff;
-    border-color: #c6e2ff;
-    background-color: #ecf5ff;
-}
-
-.p-button:active {
+  &:focus, &:hover {
+      color: #409eff;
+      border-color: #c6e2ff;
+      background-color: #ecf5ff;
+  }
+  &:active {
     color: #3a8ee6;
     border-color: #3a8ee6;
     outline: none;
+  }
+
+  // disabled
+  &.is-disabled,
+  &.is-disabled:focus,
+  &.is-disabled:hover {
+      color: #c0c4cc;
+      cursor: not-allowed;
+      background-image: none;
+      background-color: #fff;
+      border-color: #ebeef5;
+  }
 }
+// 主颜色
+.p-button-primary {
+  color: #fff;
+  background-color: #409eff;
+  border-color: #409eff;
+  &:focus, &:hover {
+    background: #66b1ff;
+    border-color: #66b1ff;
+    color: #fff;
+  }
+  &:active {
+    background: #3a8ee6;
+    border-color: #3a8ee6;
+    color: #fff;
+  }
+  // disabled
+  &.is-disabled,
+  &.is-disabled:active,
+  &.is-disabled:focus,
+  &.is-disabled:hover {
+      color: #fff;
+      background-color: #a0cfff;
+      border-color: #a0cfff;
+  }
+
+}
+
+.p-button-success {
+  color: #fff;
+  background-color: #67c23a;
+  border-color: #67c23a;
+  &:focus, &:hover {
+    background: #85ce61;
+    border-color: #85ce61;
+    color: #fff;
+  }
+  &:active {
+    background: #5daf34;
+    border-color: #5daf34;
+    color: #fff;
+  }
+}
+
+.p-button-info {
+  color: #fff;
+  background-color: #909399;
+  border-color: #909399;
+  &:focus, &:hover {
+    background: #a6a9ad;
+    border-color: #a6a9ad;
+    color: #fff;
+  }
+  &:active {
+    background: #82848a;
+    border-color: #82848a;
+    color: #fff;
+  }
+}
+
+.p-button-warning {
+  color: #fff;
+  background-color: #e6a23c;
+  border-color: #e6a23c;
+  &:focus, &:hover {
+    background: #ebb563;
+    border-color: #ebb563;
+    color: #fff;
+  }
+  &:active {
+    background: #cf9236;
+    border-color: #cf9236;
+    color: #fff;
+  }
+}
+
+.p-button-danger {
+    color: #fff;
+    background-color: #f56c6c;
+    border-color: #f56c6c;
+  &:focus, &:hover {
+    background: #f78989;
+    border-color: #f78989;
+    color: #fff;
+  }
+  &:active {
+    background: #dd6161;
+    border-color: #dd6161;
+    color: #fff;
+  }
+}
+
 </style>
